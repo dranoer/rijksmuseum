2cc54ba4c941ae98ec3ed99e69f7a3f5651c9f44
nazaninnasab <dranoer@gmail.com>
2023-06-18 17:58:38 +0200

    #4 Add detail screen components

---
+++ app/src/main/java/com/dranoer/rijksmuseum/ui/component/DetailCard.kt
@@ -0,0 +1,72 @@
+package com.dranoer.rijksmuseum.ui.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.dranoer.rijksmuseum.R
+
+@Composable
+fun DetailCard(
+    modifier: Modifier = Modifier,
+    alpha: Float,
+    artist: String?,
+    description: String?,
+) {
+    Surface(
+        modifier = modifier
+            .fillMaxWidth()
+            .alpha(alpha),
+        shape = RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp),
+        color = Color.White
+    ) {
+        Column(
+            modifier = Modifier.padding(
+                top = 20.dp,
+                start = 50.dp,
+                end = 50.dp,
+                bottom = 50.dp
+            ),
+        ) {
+            //region Description
+            Text(
+                modifier = Modifier
+                    .padding(top = 20.dp),
+                text = description ?: "",
+                style = MaterialTheme.typography.body1,
+                color = colorResource(id = R.color.black),
+            ) //endregion
+            //region Title
+            Text(
+                modifier = Modifier
+                    .padding(top = 14.dp),
+                text = "by $artist" ?: "",
+                style = MaterialTheme.typography.caption,
+                color = colorResource(id = R.color.black),
+            ) //endregion
+        }
+    }
+}
+
+//region Preview
+@Preview
+@Composable
+fun DetailCardPreview() =
+    DetailCard(
+        modifier = Modifier.requiredHeight(200.dp),
+        alpha = 40F,
+        artist = "an artist!",
+        description = "This is a description for this artwork."
+    )
+//endregion
--- app/src/main/java/com/dranoer/rijksmuseum/ui/component/DetailItem.kt
+++ app/src/main/java/com/dranoer/rijksmuseum/ui/component/DetailItem.kt
@@ -1,46 +1,66 @@
 package com.dranoer.rijksmuseum.ui.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.dranoer.rijksmuseum.ui.ArtItem
+import com.dranoer.rijksmuseum.ui.theme.RijksmuseumTheme
 
 @Composable
 fun DetailItem(item: ArtItem) {
-    Column {
-        //region Avatar
-        Image(
-            modifier = Modifier.size(40.dp),
-            painter = rememberAsyncImagePainter(item.imageUrl),
-            contentDescription = null,
-        ) //endregion
-        Spacer(modifier = Modifier.width(10.dp))
-        //region Name
-        Text(
-            text = item.id,
-            color = Color.Black,
-            style = MaterialTheme.typography.h6,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-        ) //endregion
-        Spacer(modifier = Modifier.width(10.dp))
-        //region Name
-        Text(
-            text = item.longTitle,
-            color = Color.Black,
-            style = MaterialTheme.typography.h6,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-        ) //endregion
+    BoxWithConstraints(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray)
+    ) {
+
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
+        ) {
+            //region Image
+            Image(
+                modifier = Modifier.fillMaxWidth(),
+                painter = rememberAsyncImagePainter(item.imageUrl),
+                contentDescription = "Detail image",
+            ) //endregion
+            //region Content
+            DetailCard(
+                modifier = Modifier.requiredHeight(500.dp),
+                alpha = 1F,
+                artist = item.artist,
+                description = item.description,
+            )
+            //endregion
+        }
     }
 }
+
+//region Preview
+@Preview
+@Composable
+fun HeaderView_Normal() {
+    RijksmuseumTheme() {
+        DetailItem(
+            ArtItem(
+                id = "1",
+                artist = "Artist 1",
+                title = "Title 1",
+                description = "This is a description for artwork number 1",
+                imageUrl = "",
+                headerImageUrl = "",
+            )
+        )
+    }
+}
+//endregion

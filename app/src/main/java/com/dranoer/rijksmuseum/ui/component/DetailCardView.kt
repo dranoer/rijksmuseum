cc70ee33d597376120e2f7cd74e7ba60dda0dd75
nazaninnasab <dranoer@gmail.com>
2023-06-25 23:57:10 +0200

    #8 Refactor minor

--- app/src/main/java/com/dranoer/rijksmuseum/ui/component/DetailCardView.kt
+++ app/src/main/java/com/dranoer/rijksmuseum/ui/component/DetailCardView.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dranoer.rijksmuseum.R
@@ -45,7 +46,7 @@ fun DetailCardView(
             Text(
                 modifier = Modifier
                     .padding(top = dimensionResource(id = R.dimen.size_20)),
-                text = title ?: "",
+                text = title ?: stringResource(id = R.string.empty_string),
                 style = MaterialTheme.typography.body1,
                 color = colorResource(id = R.color.black),
             ) //endregion
